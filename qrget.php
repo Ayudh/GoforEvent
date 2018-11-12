68a96ef0562c70d6f73edbcb6c0e5c2c5e09a04a
Hari G <ghari81189@gmail.com>
2018-11-12 08:14:11 +0530

    fixed typos

--- qrget.php
+++ qrget.php
@@ -5,7 +5,6 @@ require_once './vendor/autoload.php';
 use Endroid\QrCode\QrCode;
 
 $qrCode = new QrCode($_GET['email']);
-// $qrCode = new QrCode("ghari.cs@gmail.com");
 $qrCode->setLogoPath(__DIR__ . '/eventfavicon.png');
 $qrCode->setLogoWidth(100);
 header('Content-Type:'.$qrCode->getContentType());
